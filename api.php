8d326d84c75e30d0037556dbd482cc7fd47ebc0d
Jose Ruzafa <jose.ruzafa@gmail.com>
2015-07-10 00:17:39 +0200

    add task.json, controller with http service, api.php connect to DDBB, include controllers.js

--- api.php
+++ api.php
@@ -1,6 +1,45 @@
 <?php 
+header('Content-Type: application/json');
 
+/*
+** Connect to database:
+*/
+ 
+// connect to the database
+$con = mysql_connect('localhost','zend','') 
+    or die('Could not connect to the server!');
+ 
+// select a database:
+mysql_select_db('checklistapp') 
+    or die('Could not select a database.');
+ 
 
+if($_GET['action'] == 'read'){
+
+	$sql = "SELECT name, status FROM tasks";
+	 
+	// execute query:
+	$result = mysql_query($sql) 
+	    or die('A error occured: ' . mysql_error());
+	 
+	// get result count:
+	$count = mysql_num_rows($result);
+	// print "Showing $count rows:<hr/>";
+	 
+	$data = array();
+
+	// fetch results:
+	while ($row = mysql_fetch_assoc($result)) {
+	    $rowName = $row['name'];
+	    $rowStatus = $row['status'];
+	 
+		$data[] = array('name' => $rowName, 'status' => $rowStatus); 
+	}
+	
+
+	echo json_encode($data);
+
+}
 
 
 
---
+++ js/controllers.js
@@ -0,0 +1,38 @@
+var checklistApp = angular.module('checklistApp', []);
+
+function TaskController ($scope, $http){
+
+		$http.get('api.php?action=read').success(function(data, $http) {
+		$scope.tasks = data;
+  });
+
+  $scope.addTask = function( ){
+
+  	console.log('añadir task');
+  }
+
+}
+
+checklistApp.controller('TaskController', ['$scope', '$http', TaskController]);
+
+// checklistApp.controller('TaskController', function ($scope) {
+
+//   // $http.get('tasks.json').success(function(data, $http) {
+//   //   $scope.tasks = data;
+//   // });
+
+//   // // $scope.tasks = [
+//   // //   {'name': 'Clean code',
+//   // //    'status': 0},
+//   // //   {'name': 'Deploy in dev',
+//   // //    'status': 1}
+//   // // ];
+
+
+//   // function addTask(){
+
+//   // 	console.log('añadir task');
+//   // }
+
+
+// });
